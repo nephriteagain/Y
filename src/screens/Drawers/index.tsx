f56f1f46c66ca88eadbb1e790e390b003592d65c
nephrite <oneabove232@gmail.com>
2024-02-13 21:26:56 +0800

    added drawer icons

--- src/screens/Drawers/index.tsx
+++ src/screens/Drawers/index.tsx
@@ -6,7 +6,12 @@ import {
     DrawerContentComponentProps
 } from "@react-navigation/drawer";
 import type { RootParamList } from "../../types"
+
 import { FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 import DrawerHeader from "../../components/DrawerHeader";
@@ -18,7 +23,6 @@ import Bookmarks from "./Bookmarks";
 import Lists from "./Lists";
 import Spaces from "./Spaces";
 import Monetization from "./Monetization";
-
 
 const Drawer = createDrawerNavigator<RootParamList>()
 
@@ -52,29 +56,53 @@ export default function Drawers() {
             <Drawer.Screen 
             name="FrontPage" 
             component={FrontPage}        
+            // this hides the navbar button
             options={{
                 headerShown: false,
+                drawerLabelStyle: {
+                    width: 0,
+                    height: 0
+                },
+                drawerItemStyle: {
+                    width : 0,
+                    height: 0
+                }    
             }}
             />
             <Drawer.Screen
             name="Premium"
             component={Premium}
+            options={{
+                drawerIcon: () => <MaterialIcons name="workspace-premium" size={24} color="black" />
+            }}
             />
             <Drawer.Screen
             name="Bookmarks"
             component={Bookmarks}
+            options={{
+                drawerIcon: () => <Feather name="bookmark" size={24} color="black" />
+            }}
             />
             <Drawer.Screen
             name="Lists"
             component={Lists}
+            options={{
+                drawerIcon: () => <FontAwesome5 name="list-alt" size={24} color="black" />
+            }}
             />
             <Drawer.Screen
             name="Spaces"
             component={Spaces}
+            options={{
+                drawerIcon: () => <Fontisto name="mic" size={24} color="black" />
+            }}
             />
             <Drawer.Screen
             name="Monetization"
             component={Monetization}
+            options={{
+                drawerIcon: () => <MaterialIcons name="monetization-on" size={24} color="black" />
+            }}
             />
         </Drawer.Navigator>
     )
