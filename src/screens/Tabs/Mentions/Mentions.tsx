cab4f5078501f1b09801db891a2b55a328c5f9cf
nephrite <oneabove232@gmail.com>
2024-02-15 12:47:56 +0800

    mentions page

--- src/screens/Tabs/Mentions/Mentions.tsx
+++ src/screens/Tabs/Mentions/Mentions.tsx
@@ -2,8 +2,12 @@ import { View, Text } from "react-native"
 
 export default function Mentions() {
     return (
-        <View>
-            <Text>Mentions</Text>
+        <View className="p-8 items-center justify-center h-full">
+            <View className="gap-y-2">
+                <Text className="font-semibold text-4xl">Join the</Text>
+                <Text className="font-semibold text-4xl">conversation</Text>
+                <Text className="font-semibold" style={{fontSize: 16, color: 'gray'}}>When someone you're following mentions you in a post, you'll see it here.</Text>
+            </View>
         </View>
     )
 }
